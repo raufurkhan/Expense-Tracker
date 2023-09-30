d442ea571e575bfb203c3ab183c767c55d90cd19
raufurkhan <raufurkhan82@gmail.com>
2023-10-01 03:23:54 +0530

    Creating Signup Screen

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import { Button } from "react-bootstrap";
+// eslint-disable-next-line
+import SignupForm from "./components/pages/SignupForm";
+
+const App = () => {
+
+
+  return (
+    <Fragment>
+
+      <SignupForm/>
+    </Fragment>
+  );
+};
+
+export default App;
