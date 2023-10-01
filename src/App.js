3f0289916ea7335bb212cf4c44e6ad8c3a64982d
raufurkhan <raufurkhan82@gmail.com>
2023-10-02 03:08:15 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { Fragment } from "react";
-import { Button } from "react-bootstrap";
-// eslint-disable-next-line
-import SignupForm from "./components/pages/SignupForm";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Login from './components/pages/Login';
+import Products from './components/products/Products';
+import NavBar from './components/NavBar/NavBar';
 const App = () => {
 
 
   return (
-    <Fragment>
+    <Router>
+    <NavBar/>
+    <Switch>
+      <Route path="/products"><Products /></Route>
+      <Route path="/login" exact><Login /></Route>
 
-      <SignupForm/>
-    </Fragment>
+    </Switch>
+  </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -7,4 +7,6 @@ import '../node_modules/react-bootstrap/dist/react-bootstrap.min';
  import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<BrowserRouter>
+    <App />
+</BrowserRouter>);
